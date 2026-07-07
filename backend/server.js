3bdab50e21c976f9e58a1f57ad0e970ac1ca1e71
NIKHIL THAKUR <thakur.nikhil200706@gmail.com>
2026-07-07 18:58:26 +0530

    cors issue

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,12 @@ app.use(
   })
 );
 app.use(compression());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
